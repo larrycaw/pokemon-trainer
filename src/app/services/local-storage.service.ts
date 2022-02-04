bff3d4f4cbcb395773f653a91aab882dc5cf00c2
Thea Gunnarsson <thea.gunnarsson14@gmail.com>
2022-02-04 13:34:53 +0100

    genereted service to local storage

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -1,19 +1,19 @@
-import { Injectable } from "@angular/core";
-
+import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root'
 })
-export class LocalStorageService{
-    constructor(){}
-    setUser(username: string){
-        localStorage.setItem('username', username);
-    }
-    getUser(){
-        let currentUser = localStorage.getItem('username');
-        return currentUser;
-    }
-    clearLocalStorage(){
-        localStorage.clear();
-    }
+export class LocalStorageService {
+
+  constructor() { }
+  setUser(username: string){
+    localStorage.setItem('username', username);
+}
+  getUser(){
+    let currentUser = localStorage.getItem('username');
+    return currentUser;
+}
+  clearLocalStorage(){
+    localStorage.clear();
+}
 }
