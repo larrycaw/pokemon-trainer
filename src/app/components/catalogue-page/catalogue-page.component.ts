a644f2e4199df31cda12f11eaf126e2021f2a26e
Filip Hein <filip.hein@gmail.com>
2022-02-05 23:50:10 +0100

    REFACTOR: Moved tests away from landing page.
    Trainer name are now displayed on trainer page and Pokemon on catalogue

--- src/app/components/catalogue-page/catalogue-page.component.ts
+++ src/app/components/catalogue-page/catalogue-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from "../../services/pokemon.service";
+import { Pokemon } from "src/app/models/pokemon.model";
 
 @Component({
     selector: 'app-catalogue-page',
     templateUrl: './catalogue-page.component.html'
 })
 
-export class CataloguePageComponent{
-    
-}
+export class CataloguePageComponent implements OnInit {
+    constructor(
+      private readonly pokemonService: PokemonService
+    ) {}
+  
+    ngOnInit(): void {
+      this.pokemonService.fetchPokemonList();
+    }
+  
+    get pokemonList(): Pokemon[] {
+      return this.pokemonService.getPokemonList();
+    }
+  }
--- src/app/components/trainer-page/trainer-page.component.ts
+++ src/app/components/trainer-page/trainer-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
+import { TrainerService } from "src/app/services/trainer.service";
+import { Trainer } from "../../models/trainer.model";
 
 @Component({
     selector: 'app-trainer-page',
     templateUrl: './trainer-page.component.html'
 })
 
-export class TrainerPageComponent{
+export class TrainerPageComponent implements OnInit {
+    constructor(
+      private readonly trainerService: TrainerService,
+    ) {}
+
+    ngOnInit(): void {
+        this.trainerService.fetchTrainer("ash");
+      }
     
-}
+      get trainer(): Trainer[] {
+        return this.trainerService.getTrainer();
+      }
+    }
