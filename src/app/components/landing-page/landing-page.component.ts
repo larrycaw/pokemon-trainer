3f722eb28a32f34d0a3b9fb44e142d9e43df0faf
Thea Gunnarsson <thea.gunnarsson14@gmail.com>
2022-02-06 21:23:27 +0100

    feat: template to landing page

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 
 @Component({
@@ -13,5 +14,8 @@ export class LandingPageComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+  onLoginSubmit(form: NgForm): void{
+    console.log(form.value.username)
   }
 }
