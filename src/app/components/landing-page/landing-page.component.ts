26ae5faab0df88d35de4bd03eca13401b1eaa53b
Thea Gunnarsson <thea.gunnarsson14@gmail.com>
2022-02-07 11:40:20 +0100

    restoring previus, adding form and sets user in local storage

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
+import { LocalStorageService } from "src/app/services/local-storage.service";
 
 
 @Component({
@@ -9,9 +12,17 @@ import { Component, OnInit } from "@angular/core";
 // Initiates API calls for trainer and pokemon on page load.
 export class LandingPageComponent implements OnInit {
   constructor(
+    private localStorageService: LocalStorageService,
+    private router : Router
   ) {}
 
   ngOnInit(): void {
 
+  }
+  onLoginSubmit(form: NgForm): void{
+    const {username} = form.value;
+    this.localStorageService.setUser(username)
+    //this.router.navigateByUrl("")
+    //console.log(this.localStorageService.getUser())
   }
 }
